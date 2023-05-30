66dc7bce530829698121b985c735cb240a4abc3a
Giovanny692 <134340927+Giovanny692@users.noreply.github.com>
2023-05-30 16:05:55 -0500

    algo2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def sumar_numeros(numero1,numero2):
     return numero1+numero2      
-print ("elo gais") 
+print ("elo gais")
+def imprimir():
+    print ("algo") 
 a=input("numero 1")
 b=input("numero 2")
 c=sumar_numeros(a,b)
